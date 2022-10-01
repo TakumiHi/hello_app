14cd73531cbe9e19175518e4b2ef1fb108b2a183
takumi.427.1080@gmail.com <ubuntu@ip-172-31-19-250.ap-northeast-1.compute.internal>
2022-10-01 07:22:32 +0000

    Improve the rootes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # see https://guides.rubyonrails.org/routing.html
   #root 'application#hello'
-  root 'application#hola'
-  #root 'application#goodbye'
+  #root 'application#hola'
+  root 'application#goodbye'
 end
